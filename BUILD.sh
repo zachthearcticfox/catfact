65944e8300e588e5bca2884371a08d1f0e3e434d
ZachTheArcticFox <thenichollsfamily159389@outlook.com>
2025-04-26 10:44:18 +0100

    Delete BUILD.sh

--- BUILD.sh
+++
@@ -1,5 +0,0 @@
-printf "If there is a ModuleNotFoundError error, please run 'pip install site'\n"
-site_pkgs=$(python3 -c 'import site; print(site.getsitepackages()[1])')
-printf "site-packages location: $site_pkgs\n"
-
-eval "sudo cp ./catfact.py $site_pkgs/catfact.py"
